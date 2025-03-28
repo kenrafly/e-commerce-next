31c36856d19c1d11b15134572e465446b6d4d878
Rafly Ardiansyah <rafly21ardiansyah@gmail.com>
2025-03-28 15:58:37 +0700

    removed passthrough

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,5 +1,3 @@
-import { PassThrough } from "stream";
-
 export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Prostore";
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || "http://localhost:3000";
